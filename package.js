09c3e74ca9e956c4ea3971105f49ab1b116dfc36
digilord <digilord@me.com>
2015-03-25 06:40:03 -0700

    Bumped version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'digilord:pluggable-admin-view',
   summary: 'pluggable administration system',
-  version: '1.0.0',
+  version: '1.0.1',
   git: "https://github.com/morsbymechanical/meteor-pluggable-admin"
 });
 
